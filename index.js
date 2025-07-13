ad18887927a34eb9ac89caafbae0b05b1d7aaf3a
Jorge Junior <65315085+jorgebritojr@users.noreply.github.com>
2025-07-12 20:12:46 -0400

    inicital commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function rank(vitorias, derrotas) {
+    let saldo = vitorias-derrotas
+
+    if (saldo===10){
+        
+    }
+
+}
+
+console.log("O Herói tem de saldo de +"saldoVitorias + "está no nível de " + nivel)
